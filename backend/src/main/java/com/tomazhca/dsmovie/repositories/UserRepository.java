11c917adc5f1973bf30ea11c6ae7431b079538f3
Tomaz Araujo <tomazhca@gmail.com>
2022-03-11 17:26:44 -0300

    Save score

---
+++ backend/src/main/java/com/tomazhca/dsmovie/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.tomazhca.dsmovie.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tomazhca.dsmovie.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	User findByEmail(String email);
+}
